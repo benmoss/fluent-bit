benmoss/fluent-bit

commit 69fffab96630869015437a6d1d13aa1efeb0a9c7

out_kafka: make MSVC compatible (#1179)
librdkafka is already compatible for MinGW and MSVC.
For MSVC, we should avoid to link libpthread.

Signed-off-by: Hiroshi Hatake <hatake@clear-code.com>

cmake/windows-setup.cmake
@@ -60,7 +60,7 @@ set(FLB_OUT_STDOUT            Yes)
 set(FLB_OUT_LIB                No)
 set(FLB_OUT_NULL              Yes)
 set(FLB_OUT_FLOWCOUNTER       Yes)
-set(FLB_OUT_KAFKA              No)
+set(FLB_OUT_KAFKA             Yes)
 set(FLB_OUT_KAFKA_REST         No)
 
 # FILTER plugins

plugins/out_kafka/CMakeLists.txt
@@ -14,4 +14,6 @@ set(src
   kafka.c)
 
 FLB_PLUGIN(out_kafka "${src}" "rdkafka")
-target_link_libraries(flb-plugin-out_kafka -lpthread)
+if (NOT MSVC)
+  target_link_libraries(flb-plugin-out_kafka -lpthread)
+endif()

commit 4b6ecb644b8ced581f3c166a416b2462ccbe3c50

build: don't build out_kafka on Windows by default
69fffab inadvertently introduced dependency on OpenSSL (via librdkafka),
which made it impossible to run Fluent Bit on many Windows platforms
where ssleay32.dll and libeay32.dll do not exist.

This is a simple patch for the issue and should fix the "Program can't
start because SSLEAY32.dll is missing" error.

Signed-off-by: Fujimoto Seiji <fujimoto@clear-code.com>

cmake/windows-setup.cmake
@@ -63,7 +63,7 @@ set(FLB_OUT_STDOUT            Yes)
 set(FLB_OUT_LIB                No)
 set(FLB_OUT_NULL              Yes)
 set(FLB_OUT_FLOWCOUNTER       Yes)
-set(FLB_OUT_KAFKA             Yes)
+set(FLB_OUT_KAFKA              No)
 set(FLB_OUT_KAFKA_REST         No)
 
 # FILTER plugins